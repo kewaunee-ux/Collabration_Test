0701158848ed483b65390be6e35a68eceba5584a
kewaunee-ux <135124416+kewaunee-ux@users.noreply.github.com>
2025-03-10 15:03:05 +0530

    V1

--- Collabration_Testing/Assets/Plugins/OpenNewTab.jslib
+++ Collabration_Testing/Assets/Plugins/OpenNewTab.jslib
@@ -1,12 +1,81 @@
 mergeInto(LibraryManager.library, {
     OpenSceneInNewTab: function(sceneName) {
         var currentURL = window.location.href;
-        var baseURL = currentURL.split('?')[0];  // Remove any existing query parameters
-        
-        // Add or update the scene parameter
+        var baseURL = currentURL.split('?')[0];
         var newURL = baseURL + '?scene=' + UTF8ToString(sceneName);
-        
-        // Open in new tab
         window.open(newURL, '_blank');
+    },
+
+    GetURLParameter: function(parameterName) {
+        var paramName = UTF8ToString(parameterName);
+        var urlParams = new URLSearchParams(window.location.search);
+        var paramValue = urlParams.get(paramName);
+        if (paramValue === null) return "";
+        
+        var bufferSize = lengthBytesUTF8(paramValue) + 1;
+        var buffer = _malloc(bufferSize);
+        stringToUTF8(paramValue, buffer, bufferSize);
+        return buffer;
+    },
+    
+    OpenURLInNewTab: function(url) {
+        window.open(UTF8ToString(url), '_blank');
+    },
+
+    DownloadFile: function(url, filename) {
+        var link = document.createElement('a');
+        link.download = UTF8ToString(filename);
+        link.href = UTF8ToString(url);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    },
+
+    EnterFullscreen: function() {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+            document.documentElement.msRequestFullscreen();
+        }
+    },
+    
+    ExitFullscreen: function() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { // Firefox
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { // IE/Edge
+            document.msExitFullscreen();
+        }
+    },
+    
+    IsInFullscreen: function() {
+        var isInFullScreen = 
+            (document.fullscreenElement && document.fullscreenElement !== null) ||
+            (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+            (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+            (document.msFullscreenElement && document.msFullscreenElement !== null);
+            
+        return isInFullScreen;
+    },
+
+    CloseCurrentTab: function(confirm) {
+        if (!confirm || confirm && window.confirm('Close this tab?')) {
+            try {
+                window.close();
+                if (window.opener) {
+                    window.opener = null;
+                    window.open('', '_self').close();
+                }
+            } catch (e) {
+                console.log('Failed to close tab:', e);
+            }
+        }
     }
 });
--- Collabration_Testing/Assets/Scripts/SceneOpener.cs
+++ Collabration_Testing/Assets/Scripts/SceneOpener.cs
@@ -1,29 +1,151 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 using System.Runtime.InteropServices;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
 
 public class SceneOpener : MonoBehaviour
 {
-    [SerializeField]
-    private string sceneName = ""; // Name of the scene to open
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private string videoURL;
+    #region Java Script Functions
 
-/*    [SerializeField]
-    private Button openButton; // Reference to the UI button*/
-
-    // Import JavaScript function
+    // Import JavaScript functions
     [DllImport("__Internal")]
     private static extern void OpenSceneInNewTab(string sceneName);
 
-    public void OpenScene()
+    [DllImport("__Internal")]
+    private static extern void OpenURLInNewTab(string url);
+
+    [DllImport("__Internal")]
+    private static extern void DownloadFile(string url, string filename);
+
+    [DllImport("__Internal")]
+    private static extern string GetURLParameter(string parameterName);
+
+    // Import JavaScript functions
+    [DllImport("__Internal")]
+    private static extern void EnterFullscreen();
+
+    [DllImport("__Internal")]
+    private static extern void ExitFullscreen();
+
+    [DllImport("__Internal")]
+    private static extern bool IsInFullscreen();
+
+    [DllImport("__Internal")]
+    private static extern void CloseCurrentTab(bool confirm);
+
+    #endregion
+
+    private bool isFullscreen = false;
+    [SerializeField] private bool useConfirmation = true;
+
+    void Start()
     {
 #if UNITY_WEBGL && !UNITY_EDITOR
-            // Use the JavaScript plugin in WebGL builds
+            // Get the scene parameter from URL
+            string sceneToLoad = GetURLParameter("scene");
+            
+            if (!string.IsNullOrEmpty(sceneToLoad))
+            {
+                // Load the specified scene
+                SceneManager.LoadScene(sceneToLoad);
+            }
+#endif
+    }
+
+    private void Awake()
+    {
+        if (videoPlayer)
+        {
+            videoPlayer.url = videoURL;
+            videoPlayer.playOnAwake = false;
+            videoPlayer.Prepare();
+            videoPlayer.prepareCompleted += OnVideoPrepared;
+        }
+    }
+
+    public void OnVideoPrepared(VideoPlayer source)
+    {
+        videoPlayer.Play();
+    }
+
+    public void OpenScene(string sceneName)
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
             OpenSceneInNewTab(sceneName);
 #else
-        // For testing in editor
         Debug.Log("Would open scene: " + sceneName);
-        // Optionally load the scene directly in editor
-        // UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+#endif
+    }
+
+    public void OpenURL(string urlToOpen)
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            OpenURLInNewTab(urlToOpen);
+#else
+        Debug.Log("Would open URL: " + urlToOpen);
+        Application.OpenURL(urlToOpen);
+#endif
+    }
+
+    // Method to trigger when button is clicked
+    public void DownloadFile(string fileUrl)
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            // Convert Google Drive viewing URL to direct download URL if needed
+            string directUrl = ConvertToDirectDownloadLink(fileUrl);
+            // Call JavaScript function to handle download
+            DownloadFile(directUrl, "downloaded_document.pdf");
+#else
+        Debug.Log("PDF download is only supported in WebGL builds");
+#endif
+    }
+
+    private string ConvertToDirectDownloadLink(string driveUrl)
+    {
+        // If the URL is already in the direct download format, return it
+        if (driveUrl.Contains("export=download"))
+            return driveUrl;
+
+        // Convert viewing URL to direct download URL
+        // Example: https://drive.google.com/file/d/FILE_ID/view?usp=sharing
+        // to: https://drive.google.com/uc?export=download&id=FILE_ID
+        if (driveUrl.Contains("/file/d/"))
+        {
+            string fileId = driveUrl.Split(new string[] { "/file/d/" }, System.StringSplitOptions.None)[1];
+            fileId = fileId.Split('/')[0];
+            return $"https://drive.google.com/uc?export=download&id={fileId}";
+        }
+
+        return driveUrl;
+    }
+
+    public void ToggleFullscreen()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            if (IsInFullscreen())
+            {
+                ExitFullscreen();
+                isFullscreen = false;
+            }
+            else
+            {
+                EnterFullscreen();
+                isFullscreen = true;
+            }
+#else
+        Debug.Log("Fullscreen toggle only works in WebGL build");
+#endif
+    }
+
+    public void CloseTab()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            CloseCurrentTab(useConfirmation);
+#else
+        Debug.Log("Tab closing works only in WebGL build");
 #endif
     }
 }
